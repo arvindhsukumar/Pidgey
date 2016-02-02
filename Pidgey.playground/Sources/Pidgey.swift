c03dd4648223e50eb5c5183e0b6a424868169db3
Arvindh Sukumar <arvindh.sukumar@gmail.com>
2016-02-02 21:54:51 +0530

    Created playground

---
+++ Pidgey.playground/Sources/Pidgey.swift
@@ -0,0 +1,15 @@
+import Foundation
+
+enum HTTPMethod{
+    case GET
+    case POST
+    case PUT
+    case DELETE
+    case HEAD
+    case OPTION
+    case PATCH
+}
+
+public class Pidgey {
+    
+}
